2603c67357630ae2546a03ea8f01ad272815977b
SyimykMitalipove <syimyk310703@gmail.com>
2023-02-18 22:18:35 +0600

    update 2 lesson

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
-import  Header from './components/Header/Header';
-import Button from './components/Button';
-import Text from './components/Text';
-import Footer from './components/Footer';
+import TodoList from './page/TodoList/TodoList';
 function App() {
   return (
     <div className="App">
-    <Header title={'Syimyk'} />
-    <Text />
-    <Button title={'Click me'} />
-    <Button title={'Follow'} />
-    <Footer />
+      <TodoList />
     </div>
   );
 }
 
 export default App;
+
+
+////
